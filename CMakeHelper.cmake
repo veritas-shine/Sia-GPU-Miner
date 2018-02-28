e64f57b1ba6f0acd17f469deab5015d38402c555
tearsofphoenix <tearsofphoenix@icloud.com>
2018-02-28 15:44:35 +0800

    fix build on MacOS

--- CMakeHelper.cmake
+++ CMakeHelper.cmake
@@ -1,3 +1,17 @@
+macro(ADD_FRAMEWORK fwname appname)
+    find_library(FRAMEWORK_${fwname}
+            NAMES ${fwname}
+            PATHS /System/Library
+            PATH_SUFFIXES Frameworks
+            NO_DEFAULT_PATH)
+    if( ${FRAMEWORK_${fwname}} STREQUAL FRAMEWORK_${fwname}-NOTFOUND)
+        MESSAGE(ERROR ": Framework ${fwname} not found")
+    else()
+        TARGET_LINK_LIBRARIES(${appname} "${FRAMEWORK_${fwname}}/${fwname}")
+        MESSAGE(STATUS "Framework ${fwname} found at ${FRAMEWORK_${fwname}}")
+    endif()
+endmacro(ADD_FRAMEWORK)
+
 function(CheckLibrary)
     if (APPLE)
         find_library(HAVE_CURL curl)
@@ -17,7 +31,7 @@ endfunction()
 
 function(LinTargetLibrary Target)
     if(APPLE)
-        target_link_libraries(${Target} OpenCL curl)
+        target_link_libraries(${Target} curl "-framework OpenCL")
     elseif(UNIX)
         target_link_libraries(${Target} OpenCL curl)
     endif()
